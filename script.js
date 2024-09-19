7df5bb503f8aebe33f83f31c313b2229da11ba8f
Michelle <lulegonzaleznataliamichelle@gmail.com>
2024-09-18 20:54:43 -0600

    18-09-24 _ Lule Gonzalez Natalia Michelle

---
+++ script.js
@@ -0,0 +1,35 @@
+//comentario de una sola linea
+/* comentario
+multilineas*/
+
+
+//18-09-24 _ Lule Gonzalez Natalia Michelle
+
+//variables
+/* las variables guardan info 
+que se puede reutilizar*/
+//declaracion de variables
+
+
+/*Tipos de datos:
+  Cadena de tecto (string): "hola",'mundo'
+  Numero (number): 5,34,2
+  Booleanos (boolean): true, false*/
+
+//Ejemplo:
+let nombre = "Lule";
+const PI = 3.1416;
+var edad = 25;
+
+//Operadores matematicos:
+let a = 5;
+let b = 2;
+
+let suma =a+b; //7
+let resta = a-b; //3
+let multiplicacion = a*b; //10
+let division = a/b; //2.5
+
+
+
+alert("hola desde el js");
